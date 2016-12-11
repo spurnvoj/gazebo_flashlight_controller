8e55f8c1330d7cf2ceb4e3b536413df17733fbda
Vojtěch Spurný <spurnvoj@fel.cvut.cz>
2016-12-11 22:59:02 +0100

    Working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright 2012 Open Source Robotics Foundation
- * Copyright 2013 Dereck Wonnacott
+ * Copyright (C) 2012-2016 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,52 +17,76 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
-#include <gazebo/gazebo.hh>
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
-#include <gazebo/math/gzmath.hh>
+#include <gazebo/gazebo_client.hh>
 
 #include <iostream>
 
+gazebo::math::Pose offset_pose = gazebo::math::Pose(0,0.2,0,0,0,0);
+gazebo::math::Quaternion offset_rot= gazebo::math::Quaternion(0,0,3.14);
+gazebo::transport::PublisherPtr pub;
+
+void publish_light_pose(const gazebo::math::Pose &pose, const char* name){
+  gazebo::msgs::Light msg;
+  msg.set_name(name);
+
+  // Convert to a pose message
+  gazebo::msgs::Set(msg.mutable_pose(), pose);
+  pub->Publish(msg);
+}
+
+// Function is called everytime a message is received.
+void cb(ConstPosesStampedPtr &_msg){
+  
+  for(int i=0; i<_msg->pose_size(); i++){
+    if (_msg->pose(i).name()=="uav2" || _msg->pose(i).name()=="uav3"){
+      gazebo::math::Vector3 math_pos = gazebo::math::Vector3(_msg->pose(i).position().x(),
+                                                       _msg->pose(i).position().y(),
+                                                       _msg->pose(i).position().z());
+      
+      gazebo::math::Quaternion math_quat = gazebo::math::Quaternion(_msg->pose(i).orientation().x(),
+                                                       _msg->pose(i).orientation().y(),
+                                                       _msg->pose(i).orientation().z(),
+                                                       _msg->pose(i).orientation().w());
+      gazebo::math::Pose new_pose;
+      new_pose.Set(math_pos, math_quat);
+      new_pose += offset_pose;                                                
+      gazebo::math::Quaternion new_orintation = new_pose.CoordRotationAdd(offset_rot);                                                
+      new_pose.Set(new_pose.pos, new_orintation);
+      
+      if (_msg->pose(i).name()=="uav2"){
+        publish_light_pose(new_pose,"light_1");
+      }else{
+        publish_light_pose(new_pose,"light_2");
+      }
+    
+    }
+  }
+}
 
 /////////////////////////////////////////////////
 int main(int _argc, char **_argv)
 {
   // Load gazebo
-//  gazebo::load(_argc, _argv);
-  gazebo::setupClient(_argc, _argv);
+  gazebo::client::setup(_argc, _argv);
+
   // Create our node for communication
   gazebo::transport::NodePtr node(new gazebo::transport::Node());
   node->Init();
 
-  // Start transport
-  gazebo::transport::run();
-
-  // Publish to a Gazebo topic
-  gazebo::transport::PublisherPtr pub =
-    node->Advertise<gazebo::msgs::Light>("~/light");
-
+  // Listen to Gazebo world_stats topic
+  gazebo::transport::SubscriberPtr sub = node->Subscribe("~/pose/info", cb);
+  
+  pub = node->Advertise<gazebo::msgs::Light>("~/light");
+  
   // Wait for a subscriber to connect
   pub->WaitForConnection();
-
-  // Publisher loop...replace with your own code.
+  
+  // Busy wait loop...replace with your own code as needed.
   while (true)
-  {
-    // Throttle Publication
-    gazebo::common::Time::MSleep(100);
-    
-    gazebo::msgs::Light msg;
-    msg.set_name("light_1");
-
-    // Generate a pose
-    gazebo::math::Pose pose(1, 2, 3, 4, 5, 6);
-
-    // Convert to a pose message
-    gazebo::msgs::Set(msg.mutable_pose(), pose);
-
-    pub->Publish(msg);
-  }
+    gazebo::common::Time::MSleep(10);
 
   // Make sure to shut everything down.
-  gazebo::transport::fini();
+  gazebo::client::shutdown();
 }
